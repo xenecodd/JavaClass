ee583fbdd84f193091fc078a6b38e3f3b6ea202c
xenecodd <70309070+xenecodd@users.noreply.github.com>
2023-10-13 16:06:44 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.printf("Hello and welcome! \n");
+        int negative = -2147483648;
+
+        System.out.println(Math.abs(negative));
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
